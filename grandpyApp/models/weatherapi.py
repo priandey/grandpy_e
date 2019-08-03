61092776722c00389da97f0a4e4d053766ab5082
Priandey <riandeypierre@gmail.com>
2019-08-03 09:22:49 +0200

    improved wheatherapi tests and weatherapi

--- grandpyApp/models/weatherapi.py
+++ grandpyApp/models/weatherapi.py
@@ -7,20 +7,21 @@ class WeatherApi():
         self.api_link = "http://api.openweathermap.org/data/2.5/weather?"
 
     def get_weather(self, coordinates):
-        payload = {"lat":coordinates['lat'],
-                   "lon":coordinates['lon'],
-                   "APPID":self.api_key
-        }
+        payload = {"lat": coordinates['lat'],
+                   "lon": coordinates['lon'],
+                   "APPID": self.api_key
+                  }
         raw_output = requests.get(self.api_link, params=payload)
         json_output = raw_output.json()
         try:
             temp_min = round(json_output["main"]["temp_min"]-273.15, 1)
             temp_max = round(json_output["main"]["temp_max"]-273.15, 1)
+            description = json_output["weather"][0]["description"]
         except KeyError:
             return None
 
         return {
             "temp_min": temp_min,
             "temp_max": temp_max,
-            "description": json_output["weather"][0]["description"]
+            "description": description
         }
--- grandpyApp/test_weatherapi.py
+++ grandpyApp/test_weatherapi.py
@@ -12,4 +12,11 @@ class TestWeatherApi:
         responses.add(responses.GET, "http://api.openweathermap.org/data/2.5/weather?",
                       json={'coord': {'lon': 12.32, 'lat': 45.44}, 'weather': [{'id': 501, 'main': 'Rain', 'description': 'moderate rain', 'icon': '10d'}, {'id': 200, 'main': 'Thunderstorm', 'description': 'thunderstorm with light rain', 'icon': '11d'}], 'base': 'stations', 'main': {'temp': 295.19, 'pressure': 1011, 'humidity': 78, 'temp_min': 292.04, 'temp_max': 299.26}, 'visibility': 10000, 'wind': {'speed': 5.7, 'deg': 30}, 'clouds': {'all': 40}, 'dt': 1564733948, 'sys': {'type': 1, 'id': 6779, 'message': 0.0083, 'country': 'IT', 'sunrise': 1564718121, 'sunset': 1564771113}, 'timezone': 7200, 'id': 3167663, 'name': 'Sestière di Santa Croce', 'cod': 200},
                       status=200)
-        assert type(weather.get_weather({"lat":45.44,"lon":12.32})) is not None
+        assert weather.get_weather({"lat":45.44,"lon":12.32}) is not None
+
+    @responses.activate
+    def test_get_weather_error(self):
+        responses.add(responses.GET, "http://api.openweathermap.org/data/2.5/weather?",
+                      json={'error':'no output'}, status=200)
+
+        assert weather.get_weather({"lat":45.44,"lon":12.32}) is None
